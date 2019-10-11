f27c02b3b9fc2647ebe090fa6cf80f54eddd63c1
jerry-ban <One00001>
2019-10-11 16:08:13 -0400

    add deployment script

--- run.py
+++ run.py
@@ -2,9 +2,14 @@
 import connexion
 import os
 import logging
+from logging.handlers import RotatingFileHandler
 
-logging.basicConfig(level=logging.DEBUG, filename="demo_rest.log")
+log_file_name = "demo_rest.log"
+
+logging.basicConfig(level=logging.INFO)
 #logging.basicConfig(level=logging.INFO, filename="demo_rest.log")
+new_log_handler = RotatingFileHandler(log_file_name, maxBytes=5 * 1024, backupCount=5)
+logging.getLogger().addHandler(new_log_handler )
 
 port_number = int(os.getenv("PORT_NUMBER", 8831))
 deployment_env = os.getenv("DEPLOY_ENV", "development")
@@ -14,7 +19,10 @@ app = connexion.FlaskApp(__name__, specification_dir='openapi/')
 app.add_api('my_api.yaml', arguments={'title': 'Optimizion Demo Service'})
 
 application = app.app  # used for the wsgi
+application.logger.addHandler(new_log_handler )
 
 if __name__ == '__main__':
+    application.logger.handlers=[]
+    logging.basicConfig(filename=log_file_name)
     app.run(port=port_number, server= www_server, debug=True)
     #app.run(threaded=False, processes = 3, port=port_number, debug=True)
