99b50479ab96244117f4dda44cef2ea3e15e17b4
jerry-ban <One00001>
2019-10-17 15:24:52 -0400

    add log file setup in the initialize stage

---
+++ initialize/deploy_backend_ec2-user.sh
@@ -0,0 +1,191 @@
+#!/usr/bin/bash
+DEPLOY_USER="ec2-user"
+
+function initialize_worker() {
+    printf "***************************************************\n\t\tSetting up host \n***************************************************\n"
+    # Update packages
+    echo ======= Updating packages ========
+    sudo apt-get update
+
+    # Export language locale settings
+    echo ======= Exporting language locale settings =======
+    export LC_ALL=C.UTF-8
+    export LANG=C.UTF-8
+
+    # Install pip3
+    echo ======= Installing pip3 =======
+    sudo apt-get install -y python3-pip
+    sudo apt-get install -y python3-dev
+    sudo apt-get install nginx
+}
+
+function setup_python_venv() {
+    printf "***************************************************\n\t\tSetting up Venv \n***************************************************\n"
+    # Install virtualenv
+    echo ======= Installing virtualenv =======
+    pip3 install virtualenv
+
+    # Create virtual environment and activate it
+    echo ======== Creating and activating virtual env =======
+    cd /home/${DEPLOY_USER}
+
+    virtualenv venv
+    #python3 -m venv venv  #this is for python3 only
+
+    source /home/${DEPLOY_USER}/venv/bin/activate
+    # export LD_LIBRARY_PATH= /home/ubuntu/.local/lib/python3.6/site-packages/
+}
+
+function clone_app_repository() {
+    printf "***************************************************\n\t\tFetching App \n***************************************************\n"
+    # Clone and access project directory
+    echo ======== Cloning and accessing project directory ========
+    if [[ -d /home/${DEPLOY_USER}/demo_rest ]]; then
+        sudo rm -rf /home/${DEPLOY_USER}/demo_rest
+        git clone -b feature https://github.com/jerry-ban/demo.git /home/${DEPLOY_USER}/demo_rest
+        cd /home/${DEPLOY_USER}/demo_rest/
+    else
+        #git clone -b develop https://github.com/indungu/yummy-rest.git ~/yummy-rest
+        #cd ~/yummy-rest/
+        git clone -b feature https://github.com/jerry-ban/demo.git /home/${DEPLOY_USER}/demo_rest
+        cd /home/${DEPLOY_USER}/demo_rest/
+    fi
+}
+
+function setup_app() {
+    printf "***************************************************\n    Installing App dependencies and Env Variables \n***************************************************\n"
+    setup_env
+    # Install required packages
+    echo ======= Installing required packages ========
+    pip install -r requirements.txt
+    pip uninstall -y connexion
+    pip3 install connexion[swagger-ui]
+    pip3 install -I gunicorn  # force to install gunicorn to current venv folder. not use  apt-get install -y gunicorn
+}
+
+# Create and Export required environment variable
+function setup_env() {
+    echo ======= Exporting the necessary environment variables ========
+    sudo cat > /home/${DEPLOY_USER}/.env << EOF
+    export DATABASE_URL="postgres://budufkitteymek:095f0029056c313190744c68ca69d19a2e315483bc41e059b40d6d9fdccf2599@ec2-107-22-229-213.compute-1.amazonaws.com:5432/d2r8p5ai580nqq"
+    export APP_CONFIG="production"
+    export SECRET_KEY="mYd3rTyL!tTl#sEcR3t"
+    export FLASK_APP=run.py
+    export PORT_NUMBER=8831
+    export DEPLOY_ENV=8831
+EOF
+    echo ======= Exporting the necessary environment variables ========
+    source /home/${DEPLOY_USER}/.env
+}
+
+# Install and configure nginx
+function setup_nginx() {
+    printf "***************************************************\n\t\tSetting up nginx \n***************************************************\n"
+    echo ======= Installing nginx =======
+    sudo apt-get install -y nginx
+
+    # Configure nginx routing
+    echo ======= Configuring nginx =======
+    echo ======= Removing default config =======
+    sudo rm -rf /etc/nginx/sites-available/default
+    sudo rm -rf /etc/nginx/sites-enabled/default
+    echo ======= Replace config file =======
+    sudo bash -c 'cat <<EOF > /etc/nginx/sites-available/default
+    server {
+            listen 80 default_server;
+            listen [::]:80 default_server;
+
+            server_name _;
+
+            location / {
+                    # reverse proxy and serve the app
+                    # running on the localhost:8831
+                    proxy_pass http://127.0.0.1:8831/;
+                    proxy_set_header HOST \$host;
+                    proxy_set_header X-Forwarded-Proto \$scheme;
+                    proxy_set_header X-Real-IP \$remote_addr;
+                    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            }
+    }
+EOF'
+
+    echo ======= Create a symbolic link of the file to sites-enabled =======
+    sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+
+    # Ensure nginx server is running
+    echo ====== Checking nginx server status ========
+    sudo systemctl restart nginx
+    sudo nginx -t
+}
+
+# Add a launch script
+function create_launch_script () {
+    printf "***************************************************\n\t\tCreating a Launch script \n***************************************************\n"
+
+    sudo cat > /home/${DEPLOY_USER}/launch.sh <<EOF
+    #!/bin/bash
+    cd /home/${DEPLOY_USER}/demo_rest
+    source /home/${DEPLOY_USER}/.env
+    source /home/${DEPLOY_USER}/venv/bin/activate
+    pip show flask
+    /home/${DEPLOY_USER}/venv/bin/gunicorn -b :8831 run:application
+EOF
+    sudo chmod 744 /home/${DEPLOY_USER}/launch.sh
+    echo ====== Ensuring script is executable =======
+    ls -la /home/${DEPLOY_USER}/launch.sh
+}
+
+function configure_startup_service () {
+    printf "***************************************************\n\t\tConfiguring startup service \n***************************************************\n"
+
+    sudo bash -c 'cat > /etc/systemd/system/demo_rest.service <<EOF
+    [Unit]
+    Description=demo_rest startup service
+    After=network.target
+
+    [Service]
+    User=${DEPLOY_USER}
+    ExecStart=/bin/bash /home/${DEPLOY_USER}/launch.sh
+
+    [Install]
+    WantedBy=multi-user.target
+EOF'
+
+    source /home/${DEPLOY_USER}/venv/bin/activate
+    sudo chmod 664 /etc/systemd/system/demo_rest.service
+    sudo systemctl daemon-reload
+    sudo systemctl enable demo_rest.service
+    sudo systemctl start demo_rest.service
+    sudo service demo_rest status
+}
+
+function setup_log(){
+printf "***************************************************\n\t\tSetup Log \n***************************************************\n"
+LOG_FILE_FULL=/var/log/demo_rest.log
+if [ ! -f ${LOG_FILE_FULL} ]; then
+    sudo touch ${LOG_FILE_FULL}
+    sudo chmod 777 ${LOG_FILE_FULL}
+    sudo chown ${DEPLOY_USER} ${LOG_FILE_FULL}
+    sudo chgrp ${DEPLOY_USER} ${LOG_FILE_FULL}
+fi
+
+}
+# Serve the web app through gunicorn
+function launch_app() {
+    printf "***************************************************\n\t\tServing the App \n***************************************************\n"
+    sudo bash /home/${DEPLOY_USER}/launch.sh
+}
+
+######################################################################
+########################      RUNTIME       ##########################
+######################################################################
+
+initialize_worker
+setup_python_venv
+clone_app_repository
+setup_app
+setup_nginx
+create_launch_script
+configure_startup_service
+setup_log
+launch_app
