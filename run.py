8b87bf5feb3f34944a01c702ecad51e13d23ce74
jerry-ban <One00001>
2019-10-18 14:31:39 -0400

    add log file setup in the initialize stage

--- run.py
+++ run.py
@@ -13,11 +13,12 @@ log_file_name = "/var/log/demo_rest.log"
 # os.chown(log_file_name, uid, gid)
 
 
-logging.basicConfig(level=logging.INFO,filename = log_file_name)
+### logging.basicConfig(level=logging.INFO,filename = log_file_name)
+logging.basicConfig(level=logging.INFO)
 #shutil.chown(log_file_name, user="ubuntu")
 #logging.basicConfig(level=logging.INFO, filename="demo_rest.log")
 #new_log_handler = RotatingFileHandler(log_file_name, maxBytes=5*1024*1024, backupCount=5)
-new_log_handler = RotatingFileHandler(log_file_name, maxBytes=5*1024, backupCount=5)
+### new_log_handler = RotatingFileHandler(log_file_name, maxBytes=5*1024, backupCount=5)
 
 #logging.getLogger().addHandler(new_log_handler )
 
@@ -29,10 +30,10 @@ app = connexion.FlaskApp(__name__, specification_dir='openapi/')
 app.add_api('my_api.yaml', arguments={'title': 'Optimizion Demo Service'})
 
 application = app.app  # used for the wsgi
-application.logger.addHandler(new_log_handler )
+#application.logger.addHandler(new_log_handler )
 
 if __name__ == '__main__':
     #application.logger.handlers=[]
     #logging.basicConfig(filename=log_file_name)
-    app.run(port=port_number, server= www_server, debug=True)
+    app.run(port=port_number, debug=True)
     #app.run(threaded=False, processes = 3, port=port_number, debug=True)
