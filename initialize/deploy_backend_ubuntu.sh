81433cd5698ae7902eab7c010307e8f78b83f011
jerry-ban <One00001>
2019-10-24 21:35:41 -0400

    add docker tutorial documents

--- initialize/deploy_backend_ubuntu.sh
+++ initialize/deploy_backend_ubuntu.sh
@@ -15,7 +15,7 @@ function initialize_worker() {
     echo ======= Installing pip3 =======
     sudo apt-get install -y python3-pip
     sudo apt-get install -y python3-dev
-    sudo apt-get install nginx
+    # sudo apt-get install nginx
 }
 
 function setup_python_venv() {
